9c0e1616c88f19d3eaacb15c0ecc92100ba14608
zmar0519 <zachary.p.martin7@gmail.com>
2021-07-17 15:30:26 -0500

    combine two  models

--- models/profile.js
+++ models/profile.js
@@ -1,15 +1,27 @@
 import mongoose from 'mongoose'
 
 export {
-  Profile
+  Profile,
+  game
 }
+
+const gameSchema = new mongoose.Schema({
+  name: String,
+  avatar: String,
+  releaseDate: Date,
+  genre: String,
+  rating: String,
+}, {
+  timestamps: true
+})
 
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
-  // game: [completedGameSchema]
+  game: [gameSchema] // Ref to gameSchema
 }, {
   timestamps: true
 })
 
 const Profile = mongoose.model('Profile', profileSchema)
+const game = mongoose.model('game', gameSchema)
