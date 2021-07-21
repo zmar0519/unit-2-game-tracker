fefb3d41aa5df80b7e10f991c8671adb8b07b1a4
zmar0519 <zachary.p.martin7@gmail.com>
2021-07-21 15:45:16 -0500

    create show profile page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,6 +2,23 @@ import { Profile } from "../models/profile.js"
 
 export {
   index,
+  show,
+}
+
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile._id)
+    .then(self => {
+      const isSelf = self._id.equals(profile._id)
+      res.render("profiles/show", {
+        title: `${profile.name}'s profile`,
+        profile,
+        self,
+        isSelf,
+      })
+    })
+  })
 }
 
 function index(req, res) {
